e3fdf07b866f4b02c72fccdeeec3e209bd5c7278
jakegornall <jakegornall@yahoo.com>
2016-11-16 10:58:59 -0500

    initial commit

---
+++ static/js/app.js
@@ -0,0 +1,47 @@
+// initiates the map with user's current location.
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 6,
+        disableDefaultUI: true
+    });
+    var infoWindow = new google.maps.InfoWindow({map: map});
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+}
+
+
+// Knockout.js ViewModel
+var viewModel = {
+    numberOfClicks : ko.observable(0),
+    incrementClickCounter : function() {
+        var previousCount = this.numberOfClicks();
+        this.numberOfClicks(previousCount + 1);
+    }
+};
+
+ko.applyBindings(viewModel);
