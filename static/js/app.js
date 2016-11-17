ed0c3da0efe6719897df0e398d547813e2498ba5
jakegornall <jakegornall@yahoo.com>
2016-11-17 18:47:27 -0500

    integrate GoogleMaps and Knockout

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 // initiates the map with user's current location.
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
+        center: {lat: -25.363, lng: 131.044},
         zoom: 6,
         disableDefaultUI: true
     });
@@ -18,6 +18,7 @@ function initMap() {
             infoWindow.setPosition(pos);
             infoWindow.setContent('Location found.');
             map.setCenter(pos);
+            viewModel.mapCenter(pos);
         }, function() {
             handleLocationError(true, infoWindow, map.getCenter());
         });
@@ -25,6 +26,47 @@ function initMap() {
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
     }
+
+
+    // Knockout.js ViewModel
+    var viewModel = {
+        mapCenter : ko.observable(),
+        places : ko.observableArray(),
+        mainWindowState : ko.observable(false),
+        mainWindowControl : function() {
+            if (this.mainWindowState() === false) {
+                $mainWindow.animate({
+                    top: "25%"
+                });
+                this.mainWindowState(true);
+            } else {
+                $mainWindow.animate({
+                    top: mainWindowClosedPos.top
+                });
+                this.mainWindowState(false);
+            }
+        },
+        addNewPlace : function(formElement) {
+            var title = $newPlaceTextbox.val();
+            var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+            var marker = new google.maps.Marker({
+                position: myLatlng,
+                title:"Hello World!"
+            });
+
+            marker.addListener('click', function() {
+                map.setZoom(8);
+                map.setCenter(marker.getPosition());
+            });
+            
+            var newPlace = new Place(title, myLatlng);
+            this.places.push(newPlace);
+            // To add the marker to the map, call setMap();
+            marker.setMap(map);
+            map.panTo(myLatlng);
+        }
+    }
+    ko.applyBindings(viewModel);  
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -34,46 +76,15 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         'Error: Your browser doesn\'t support geolocation.');
 }
 
-(function() {
-    var _dragged;
-    ko.bindingHandlers.drag = {
-        init: function(element, valueAccessor, allBindingsAccessor, viewModel) {
-            var dragElement = $(element);
-            var dragOptions = {
-                helper: 'clone',
-                revert: true,
-                revertDuration: 0,
-                start: function() {
-                    _dragged = ko.utils.unwrapObservable(valueAccessor().value);
-                },
-                cursor: 'default'
-            };
-            dragElement.draggable(dragOptions).disableSelection();
-        }
-    };
+// Caches repeatedly accessed objects.
+var $mainWindow = $('#main-ui-window-bottom');
+var mainWindowClosedPos = $mainWindow.offset();
+var $newPlaceTextbox = $('#new-place-textbox');
 
-    ko.bindingHandlers.drop = {
-        init: function(element, valueAccessor, allBindingsAccessor, viewModel) {
-            var dropElement = $(element);
-            var dropOptions = {
-                drop: function(event, ui) {
-                    valueAccessor().value(_dragged);
-                }
-            };
-            dropElement.droppable(dropOptions);
-        }
-    };
-})();
 
-// Knockout.js ViewModel
-var viewModel = {
-    mainWindowDrag : function(MainWindow, event) {
-        var dragOptions = {
-                start: function() {
-                    var _dragged = ko.utils.unwrapObservable(valueAccessor().value);
-                },
-                cursor: 'default'
-            };
-        $('#main-ui-window-bottom').draggable(dragOptions);
-    }
+// MODEL DEFINITIONS:
+function Place(title, pos) {
+	// holds all the data for a single place.
+    this.title = title;
+	this.pos = pos;
 }
